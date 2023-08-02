0cf30de39aecca7c8ef58e1866bd6ab1610b94f7
KingHarley <cweiss1995@gmail.com>
2023-08-03 00:35:18 +0800

    Add logic to get cells new state

--- Life/Cell.cs
+++ Life/Cell.cs
@@ -21,5 +21,24 @@ namespace Life
             State = state;
             Rectangle = rectangle;
         }
+
+        public State GetNewCellState(State[] neighbours)
+        {
+            if (neighbours.Count() > 4)
+                throw new Exception($"Cell cannot have more than 4 neighbours. Something went wrong.");
+
+            if (neighbours.Count() < 2)
+                throw new Exception($"A cell cannot have less than 2 neighbours. Something went wrong.");
+
+            var aliveNeighbours = neighbours.Where(s => s == State.Alive).Count();
+
+#pragma warning disable 8524
+            return State switch
+            {
+                State.Alive => (aliveNeighbours == 2 || aliveNeighbours == 3) ? State.Alive : State.Dead,
+                State.Dead => aliveNeighbours == 3 ? State.Alive : State.Dead
+            };
+#pragma warning restore 8524
+        }
     }
 }
